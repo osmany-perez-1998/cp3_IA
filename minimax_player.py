4b8babec5844720109a2e8791d57176d4f8b5ff1
Osmany <osmanypr2013@gmail.com>
2022-04-05 16:47:36 -0400

    refractor: minimax player DONE

--- minimax_player.py
+++ minimax_player.py
@@ -47,77 +47,38 @@ def moves(game, player):
 
 
 def heuristic(game, player):
-	if player == PLAYER[0]:
-		# print(1 - (bfs_white(game,player)/(game.size**2)))
-		return 1 - (bfs_white(game,player)/(game.size**2))
-	else:
-		# print(1 - (bfs_black(game,player)/(game.size**2)))
-		return 1 - (bfs_black(game,player)/(game.size**2))
-	# return 0
+	return 1 - (bfs(game,player)/(game.size**2))
+
 
 def desired_neighbour(game:Game, player,x,y):
-	# neig = list(game.neighbour(x, y))
 	for i,j in game.neighbour(x, y):
 		if game[i,j] == EMPTY or game[i,j]== player:
 			yield (i,j)
 
 
-def bfs_black(game:Game,player):
+def bfs(game:Game,player):
 	q = []
 	v = {}
 
-	for j in range(game.size):
-		if game[0,j] == EMPTY:
-			q.append(((0,j),1,None))
-			v[(0,j)]=(1,None)
-		elif game[0,j] == player:
-			q.append(((0,j),0,None))
-			v[(0,j)]=(0,None)
-			
-
-	while q:
-		place = q.pop(0)
-
-		if place[0][0] == game.size -1:
-			return place[1]
-
-		x,y = place[0]
-		aux =[]
-		for neig in desired_neighbour(game,player,x,y):
-			try:
-				v[neig]
-			except KeyError:
-				if game[neig[0],neig[1]] == EMPTY:
-					aux.append(((neig[0],neig[1]),place[1]+1, place[0]))
-					v[neig] = (place[1]+1,place[0])
-				else:
-					aux.insert(0,((neig[0],neig[1]),place[1], place[0]))					
-					v[neig] = (place[1],place[0])					
-		
-		q.extend(aux)
-
-
-def bfs_white(game:Game,player):
-	q = []
-	v = {}
 
 	for j in range(game.size):
+		a,b =j,0
+		if player == PLAYER[1]:
+			a,b = 0,j
 		
-		if game[j,0] == EMPTY:
-			q.append(((j,0),1,None))
-			v[(j,0)]=(1,None)
-		elif game[j,0] == player:
-			q.append(((j,0),0,None))
-			v[(j,0)]=(0,None)
+		if game[a,b] == EMPTY:
+			q.append(((a,b),1,None))
+			v[(a,b)]=(1,None)
+		elif game[a,b] == player:
+			q.append(((a,b),0,None))
+			v[(a,b)]=(0,None)
 			
 
 	while q:
 		place = q.pop(0)
 
 		x,y = place[0]
-		if place[0][1] == game.size -1:
-			# print("Dictionary.")
-			# print(v)
+		if place[0][1 if player == PLAYER[0] else 0] == game.size -1:
 			return place[1]
 
 		aux =[]
